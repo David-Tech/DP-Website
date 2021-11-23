b6317e1cb664a2e1ffa83483fb92d817189e62d5
David-Tech <dbriken.tech@gmail.com>
2021-11-23 00:19:33 -0500

    initial commit

---
+++ dp.js
@@ -0,0 +1,17 @@
+var nav = document.getElementById('topnav');
+var navlinks = nav.getElementsByTagName('a');
+
+function toggleNav() {
+    (nav.classList.contains('active')) ? nav.classList.remove('active') : nav.classList.add('active');
+  }
+
+document.getElementById('btn').addEventListener('click', function(e) {
+    e.preventDefault();
+    toggleNav();
+});
+
+for(var i = 0; i < navlinks.length; i++) {
+    navlinks[i].addEventListener('click', function() {
+      toggleNav();
+  });
+}
